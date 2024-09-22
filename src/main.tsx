55358ee8a99b03cb3efb2a8a0f62c33670ca9e78
Louis Tran <legendofall18@gmail.com>
2024-09-22 15:15:39 +0700

    Change the name of the folder app to redux

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
-import { store } from "./app/store";
+import { store } from "./redux/store";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./configs/routes";
